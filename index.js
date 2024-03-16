319be11da0a8afe3b8cf0360367bff541c32f094
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-16 20:28:16 +0530

    again

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-const mongoClient = require('mongodb').MongoClient
-const url = ""
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
