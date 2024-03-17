ec4788fe22f5378b7ced95065b9220db5fb49aa5
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-17 10:40:09 +0530

    111

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require('express')
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose')
 const app = express()
 const bodyParser = require('body-parser'); 
 const PORT = 3000
 const uri = "mongodb+srv://sarvesh:mevo123@testingcluster.tg9uqrx.mongodb.net/?retryWrites=true&w=majority&appName=TestingCluster";
-const client = new MongoClient(uri);
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: false })); 
+
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(uri);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+  
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
 
-client.connect(err => {
-    if(err){ console.error(err); return false;}
+connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("listening for requests");
     })
-});
+})
