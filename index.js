154c9abe6aa4c9a9cd3b0222197ad0dd4e6fd1af
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-18 14:36:10 +0530

    try1

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 //const mongoose = require('mongoose')
-const mongodb = require('mongodb')
+const { MongoClient } = require("mongodb");
 const app = express()
 const bodyParser = require('body-parser'); 
 const PORT = 3000
 const uri = "mongodb+srv://sarvesh:mevo123@testingcluster.tg9uqrx.mongodb.net/?retryWrites=true&w=majority&appName=TestingCluster";
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: false })); 
-
-
+const client = new MongoClient(uri);
+const database = client.db('mevo');
+const users = database.collection('users');
 
 /*
 const connectDB = async () => {
@@ -28,9 +29,20 @@ app.all('/', (req, res) => {
 })
 
 app.post('/signup',(req,res) => {
-  const user = req.body
+  //const user = req.body
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }
   console.log(user)
-  res.send("Success")
+  const result = users.insertOne(user);
+  if(result != null){
+    res.send("Success")
+  } else {
+    res.send("failed")
+  }
+
   //obj = { id : 1, name : "shankar" }
   //res.end(JSON.stringify(obj))
 })
