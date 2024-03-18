f5a22f380f28f862196bf29a2c4054f6276b2e75
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-18 22:23:06 +0530

    2

--- index.js
+++ index.js
@@ -66,9 +66,9 @@ app.post('/AddRoom', async(req,res) => {
   }
 })
 
-app.post('/GetRooms', async(req,res) => {
+app.get('/GetRooms', async(req,res) => {
   const rooms = database.collection('rooms')
-  const result = await rooms.find()
+  const result = await rooms.find({})
   console.log(result)
   if(result != null){
     res.status(200).send(JSON.stringify(result))
