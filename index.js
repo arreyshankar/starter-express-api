d32e69f8803e955e024d8e456249e370ff360e29
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-03-19 09:03:22 +0530

    1

--- index.js
+++ index.js
@@ -70,12 +70,9 @@ app.get('/GetRooms', async(req,res) => {
   const rooms = database.collection('rooms')
   const result = await rooms.find().toArray()
   if(result != null){
-    var obj = JSON.stringify(Object.assign({}, result))
-    console.log(obj)
     res.status(200).send(JSON.stringify(Object.assign({}, result)))
   } else if(result == null){
-    var obj = { message: "Error Getting rooms" }
-    res.status(201).send(JSON.stringify(obj))
+    res.status(201).send(JSON.stringify({ message: "Error Getting rooms" }))
   }
 })
 
